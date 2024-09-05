e43b182316da2ef407235b467322ac89975e497a
pgomezzy <palp53412@gmail.com>
2024-09-04 23:22:47 -0300

    Termino do Desafio

--- src/ContarExemplo.java
+++ src/ContarExemplo.java
@@ -1,19 +1,13 @@
-import java.util.Scanner;
-public class Contador {
+public class ContarExemplo {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parametro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parametro");
-        int parametroDois = terminal.nextInt();
 
     try {
-            contar(parametroUm,parametroDois);
+            contar(2,5);
 
     } catch (ParametrosInvalidosException exception) {
-        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        System.out.println("loucura");
     }
-}
+    }
     static void contar(int parametroUm , int parametroDois) throws ParametrosInvalidosException{
         if (parametroUm > parametroDois){
             throw new ParametrosInvalidosException();
@@ -22,8 +16,6 @@ public class Contador {
             for (int indice = parametroUm ; indice <= parametroDois ;indice++){
                 System.out.println(indice);
             }
-        
-        }
-     }
+    }
+}
     
-
