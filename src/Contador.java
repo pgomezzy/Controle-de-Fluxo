e5a1a8f280febd4807e538e9497053fe249aecbd
pgomezzy <palp53412@gmail.com>
2024-09-04 22:11:21 -0300

    Scanner adicionado

---
+++ src/Contador.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parametro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parametro");
+        int parametroDois = terminal.nextInt();
+
+
+    }
+        
+    }
+}
